448eaf83bd4f37deb9d6551c191e368d7c062a78
GiovannaK <giovannakelli@gmail.com>
2023-04-04 22:35:04 -0300

    inferencia

---
+++ clinica/src/people/Medico.java
@@ -0,0 +1,5 @@
+package clinica.src.people;
+
+public class Medico {
+    
+}
---
+++ clinica/src/people/Paciente.java
@@ -0,0 +1,11 @@
+package clinica.src.people;
+
+
+/*
+ * @param args
+ * @return
+ * @throws
+*/
+public class Paciente {
+    
+}
