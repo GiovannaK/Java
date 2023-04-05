dd70d8830ed61135352b282f788f1118acd82764
GiovannaK <giovannakelli@gmail.com>
2023-04-04 22:48:40 -0300

    primitivos

---
+++ fundamentos/Primitivos.java
@@ -0,0 +1,32 @@
+package fundamentos;
+
+public class Primitivos {
+    
+}
+
+/*
+ * Primitivos
+ * 6 numericos
+ * Byte(1 byte), Short(2 byte), Int(4 byte), Long(8 byte)
+ * Byte -> -128 a 127
+ * Short -> -32768 a 32767
+ * Int -> -2147483648 a 2147483647
+ * Long -> -9223372036854775808 a 9223372036854775807
+ * Double -> 8 bytes -> casas decimais
+ * Float -> 4 bytes -> casas decimais
+ * 1 byte = 8 bits
+ * 
+ * booleanos
+ * boolean -> true or false
+ * 
+ * char
+ * char -> '' -> 'a'
+ * 
+ * Literais inteiros serão int por padrão
+ * Literais reais serão double por padrão
+ * 
+ * tipos menores cabem em tipos maiores na conversão
+ * tipos maiores não cabem em tipos menores na conversão
+ * portanto, é necessário fazer a conversão com cuidado para 
+ * não perder dados
+*/
---
+++ fundamentos/TiposPrimitivos.java
@@ -0,0 +1,33 @@
+package fundamentos;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        // Informações do funcionário
+        // Tipo primitivo byte
+        byte anosDeEmpresa = 23;
+        // Tipo primitivo short
+        short numeroDeVoos = 542;
+        // Tipo primitivo int
+        int id = 56789;
+        // Tipo primitivo long
+        long pontosAcumulados = 3_134_845_223L;
+        // Tipo primitivo float
+        float salario = 11_445.44F;
+        // Tipo primitivo double
+        double vendasAcumuladas = 2_991_797_103.01;
+        // Tipo primitivo boolean
+        boolean estaDeFerias = false;
+        // Tipo primitivo char
+        char status = 'A'; // ativo
+
+        // Dias de empresa
+        System.out.println(anosDeEmpresa * 365);
+        // Número de viagens
+        System.out.println(numeroDeVoos / 2);
+        // Pontos por real
+        System.out.println(pontosAcumulados / vendasAcumuladas);
+        System.out.println(id + ": ganha -> " + salario);
+        System.out.println("Férias? " + estaDeFerias);
+        System.out.println("Status: " + status);
+    }
+}
