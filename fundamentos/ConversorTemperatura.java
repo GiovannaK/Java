c85f5460dd336ed4b992da50490986fcb916659c
GiovannaK <giovannakelli@gmail.com>
2023-04-05 12:47:31 -0300

    a

--- fundamentos/ConversorTemperatura.java
+++ fundamentos/ConversorTemperatura.java
@@ -4,7 +4,7 @@ public class ConversorTemperatura {
     public static void main(String[] args) {
         final double FATOR = 5.0 / 9.0;
         final double AJUSTE = 32;
-        
+        //test for a branch
         double fahrenheit = 86;
         double celsius = (fahrenheit - AJUSTE) * FATOR;
         
