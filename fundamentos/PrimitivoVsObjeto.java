300248b851ec7602fd2fdc1fe112e700e3d682d1
GiovannaK <giovannakelli@gmail.com>
2023-05-01 22:02:15 -0300

    primitivos

---
+++ fundamentos/PrimitivoVsObjeto.java
@@ -0,0 +1,13 @@
+package fundamentos;
+
+public class PrimitivoVsObjeto {
+    public static void main(String[] args) {
+        String s = new String("texto");
+        s.toUpperCase();
+        
+        //primitivo carrega apenas o proprio valor
+        int a = 123;
+
+        // Wrapper versão objeto do tipo primitivo
+    }
+}
