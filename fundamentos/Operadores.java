097022f961b297eed9bc566fa05e1ac9185050de
GiovannaK <giovannakelli@gmail.com>
2023-05-01 22:36:21 -0300

    operadores

---
+++ fundamentos/Operadores.java
@@ -0,0 +1,21 @@
+package fundamentos;
+
+public class Operadores {
+    public static void main(String[] args) {
+        // Operadores Aritméticos
+        // Binários - Numeros antes e depois do operador
+        // 2+2
+        //Unários - Só um numero antes do operador
+        //a++
+
+        //Ternário - 3 operandos
+        // a ? b : c
+
+        // Prefix 
+        // ++a
+        // Postfix
+        // a++
+        // Infix
+        // a + b
+    }
+}
